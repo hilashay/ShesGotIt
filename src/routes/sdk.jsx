a4302442ac1a4983cc6c2388d4d261c999b15a95
Hila Shay <hilashay17.3@gmail.com>
2022-07-03 17:23:42 +0300

    before review task

---
+++ src/routes/sdk.jsx
@@ -0,0 +1,14 @@
+<body>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : 'your-app-id',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v14.0'
+    });
+  };
+</script>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
+ 
+</body>
